3f3c02377ee1ee0bd0c811d399abbe1a8a7e34a8
CiviJS <jeiderarmansolano@gmail.com>
2026-07-06 17:26:25 -0500

    fix dockerfile & entrypoint: pequeño ajuste de permisos

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
+if [ ! -d /var/www/database ]; then
+    mkdir -p /var/www/database
+fi
+
+if [ ! -f /var/www/database/database.sqlite ]; then
+    touch /var/www/database/database.sqlite
+fi
+
 chown -R www-data:www-data /var/www/database
 chmod -R 775 /var/www/database
-
-
 echo "Ejecutando migraciones ACTUALIZADO OJO SINO SALE ESTO ENTONCES NO ESTA USANDO ESTA VERSION."
 php artisan migrate --force
 
